0664ef3c194ef61d7bd4375760ea0fafea2cd760
arty <sverrevanhoepen12@outlook.com>
2022-09-20 08:22:14 +0200

    createdBy Invite

--- routes/AdminRoute.ts
+++ routes/AdminRoute.ts
@@ -3,18 +3,30 @@ const router = express.Router();
 import db from "../models"
 import {} from "../types/admin"
 
-router.get("/CreateInvite", async (req: express.Request, res: express.Response) => {
+router.post("/CreateInvite", async (req: express.Request, res: express.Response) => {
     if (!req.isAuthenticated() || !req.user?.isAdmin) { res.cookie("notification", "You are not allowed to access this page!"); return res.redirect("/")};
-    const invite = await db.Invites.create({
-        code: "arty_" + Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15),
-        createdBy: null,
-        usedBy: null,
-        isUsed: false,
-    });
-
-    res.cookie("clipboard", invite.code);
-
-    return res.cookie("notification", "Invite code: " + invite.code).redirect("/dashboard");
+    if (req.body.amount){
+        for (let i = 0; i < req.body.amount; i++) {
+            db.Invites.create({
+                code: "arty_" + Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15),
+                createdBy: req.user.username,
+                usedBy: null,
+                isUsed: false,
+            },);
+        }
+        return res.cookie("notification", "Generated: " + req.body.amount + " invites").redirect("/dashboard");
+    } else {
+        const invite = await db.Invites.create({
+            code: "arty_" + Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15),
+            createdBy: req.user.username,
+            usedBy: null,
+            isUsed: false,
+        });
+    
+        res.cookie("clipboard", invite.code);
+    
+        return res.cookie("notification", "Invite code: " + invite.code).redirect("/dashboard");
+    }
 });
 
 export default router;
